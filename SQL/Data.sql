1074cc7faa4bddd027fa31b24e336d6d1128f13a
Steven <maharostevraj@gmail.com>
2023-05-08 11:14:01 +0300

    SQL files

---
+++ SQL/Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO type_profil (nom) VALUES ('Administrateur');
+INSERT INTO profil (idtype_profil, nom, email, password) VALUES (1, 'Admin', 'admin@gmail.com', md5('admin'));
---
+++ SQL/Table.sql
@@ -0,0 +1,37 @@
+CREATE  TABLE "public"."categorie" (
+	id                   serial  NOT NULL  ,
+	nom              	 varchar(50)  NOT NULL  ,
+	CONSTRAINT pk_categorie PRIMARY KEY ( id )
+ );
+
+CREATE  TABLE "public".contenu (
+	id                   serial  NOT NULL  ,
+	titre                varchar(50)  NOT NULL  ,
+	visuel               text    ,
+	body                 text    ,
+	date1          		 timestamp  NOT NULL  ,
+	date2             	 timestamp   ,
+	lieu                 varchar(50)    ,
+	idprofil 	         integer not null,
+	idcategorie 	     integer not null,
+	CONSTRAINT pk_contenu PRIMARY KEY ( id )
+ );
+
+CREATE  TABLE "public"."profil" (
+	id                   serial  NOT NULL  ,
+	idtype_profil		 integer not null,
+	nom                  varchar(30)  NOT NULL  ,
+	email                varchar(50)  NOT NULL  ,
+	"password"           varchar(255)  NOT NULL  ,
+	CONSTRAINT pk_profil PRIMARY KEY ( id )
+ );
+
+CREATE  TABLE "public"."type_profil" (
+	id                   serial  NOT NULL  ,
+	nom              	 varchar(50)  NOT NULL  ,
+	CONSTRAINT pk_type_profil PRIMARY KEY ( id )
+ );
+
+ALTER TABLE profil ADD FOREIGN KEY idtype_profil REFERENCES type_profil (id);
+ALTER TABLE contenu ADD FOREIGN KEY idprofil REFERENCES profil (id);
+ALTER TABLE contenu ADD FOREIGN KEY idcategorie REFERENCES categorie (id);
